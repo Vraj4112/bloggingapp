83ff00ca4f7ae667414f89ecb12d6a28c6a1abe5
Vraj4112 <vrajspatel99250@gmail.com.com>
2025-02-11 18:06:35 +0530

    promises

--- src/modules/blogs/routes.js
+++ src/modules/blogs/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 const {
   uploadFileToVercelBlob,
 } = require("../../utilities/uploadFileToVercelBlob");
